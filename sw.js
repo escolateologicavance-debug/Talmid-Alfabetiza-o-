e302a76bd6feae5f4775c10faf9317e0bd347346
Gilberto Moreira <escolateologicavance@gmail.com>
2026-03-18 11:50:28 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'talmid-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './1.html',
+  './2.html',
+  './3.html',
+  './4.html',
+  './5.html',
+  './6.html',
+  './7.html',
+  './logo-192.png',
+  './logo-512.png',
+  'https://fonts.googleapis.com/css2?family=Alef:wght@400;700&display=swap'
+];
+
+// Instalação e Cache dos arquivos
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Ativação e limpeza de caches antigos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Estratégia: Cache First, Fallback to Network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
